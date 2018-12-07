c04886c8dc45b5d07ecb67f7bcb05bcfff0ae43c
zxz <zxz@ayohei.com>
2018-12-07 15:36:50 +0800

    demo 修改

--- test/public/indexTest.php
+++ test/public/indexTest.php
@@ -2,6 +2,7 @@
 include '../../vendor/autoload.php';
 
 \zxzgit\swd\WebSocketApp::run([
+    'debugOn' => true,
     'moduleList' => [
         'test' => \zxzgit\swd\test\modules\test\MessageModule::class,
     ],
@@ -11,19 +12,19 @@ include '../../vendor/autoload.php';
 
         },
         'workerStart' => function (&$server, $id) {
-            echo PHP_EOL . "workerStart event" . PHP_EOL;
+            //echo PHP_EOL . "workerStart event" . PHP_EOL;
         },
         'open' => function (&$server, &$req) {
-            echo PHP_EOL . "open event" . PHP_EOL;
+            //echo PHP_EOL . "open event" . PHP_EOL;
         },
         'beforeMessage' => function (&$server, &$frame) {
-            echo PHP_EOL . "beforeMessage event" . PHP_EOL;
+            //echo PHP_EOL . "beforeMessage event" . PHP_EOL;
         },
         'afterMessage' => function (&$server, &$frame) {
-            echo PHP_EOL . "afterMessage event" . PHP_EOL;
+            //echo PHP_EOL . "afterMessage event" . PHP_EOL;
         },
         'close' => function (&$server, $fd) {
-            echo PHP_EOL . "afterMessage event" . PHP_EOL;
+            //echo PHP_EOL . "afterMessage event" . PHP_EOL;
         },
     ]
 ]);
